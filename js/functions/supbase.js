9ab54237ed080c3f862e222503c57288dd8ce00a
joostsijm <joostsijm@gmail.com>
2016-11-03 22:50:42 +0100

    Moving projects in their own folder, making code reusable

---
+++ js/functions/supbase.js
@@ -0,0 +1,152 @@
+// tools.js
+// ========
+module.exports = (function() {
+	var Nightmare = require('nightmare')
+	require('nightmare-window-manager')(Nightmare);
+
+	var showWindow = true;
+	var debug = true;
+
+	// App data
+	var useragent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36";
+	var nightmare_opts = {
+		show: showWindow,
+		waitTimeout: 10000,
+		gotoTimeout: 5000,
+		loadTimeout: 5000,
+	};
+
+	var suploginurl = "http://www.supremacy1914.com/index.php?id=250&L=2";
+
+	var nightmare = Nightmare(nightmare_opts);
+	nightmare.useragent(useragent);
+	function RunNext(day, totaldays, indexdays) {
+		if (day <= totaldays) {
+			if(debug) { console.log("Getting power index of day: " +  day); }
+			nightmare
+				.evaluate(function() {
+					document.querySelector('input#func_newspaper_day_tf').value = ''
+				})
+				.type('input[id=func_newspaper_day_tf]', day)
+				.wait(500)
+				.click('div[id=func_newspaper_ranking_show_all_button]')
+				.wait(500)
+				.evaluate(function() {
+					var data = []
+					for (player of document.querySelectorAll('div#newspaper_ranking_single ol li')) {
+						playerdata = [];
+						playerdata[0] = player.querySelector('div.autoResizeLine').innerHTML.trim()
+						playerdata[1] = player.querySelector('div.ranking_points').innerHTML.trim()
+						data.push(playerdata)
+					}
+					return data
+				})
+				.then(function(dayindex) {
+					indexdays.push(dayindex)
+				});
+			nightmare
+				.wait(1500)
+				.run(function() {
+					RunNext(day+1, totaldays, indexdays);
+				});
+		}
+		else {
+			processdays(indexdays)
+		}
+	}
+
+	return {
+		Login: function (username, password) {
+			return new Promise(function(resolve, reject) {
+				if(debug) { console.log("[DEBUG] Handle login page: " + username); }
+				nightmare
+					.windowManager()
+					.goto(suploginurl)
+					.exists('form[id=sg_login_form]')
+					.then(function(nologin) {
+						if (nologin) {
+							if(debug) { console.log("[DEBUG] Not logged in.") }
+							nightmare
+								.type('form[id=sg_login_form] [id=user]', username)
+								.type('form[id=sg_login_form] [id=pass]', password)
+								.click('form[id=sg_login_form] [type=submit]')
+								.waitWindowLoad()
+							if(debug) { console.log("[DEBUG] Filled in form."); }
+						}
+						else if(debug) { console.log("[DEBUG] Already logged in.") }
+						resolve("Authorization succesfull.");
+					});
+			});
+		},
+		GetGameUrl: function(vargameid) {
+			return new Promise(function(resolve, reject) {
+				if(debug) { console.log("[DEBUG] Opening the game: " + vargameid); }
+				nightmare
+					.type('input[id=sg_game_search_field]', vargameid)
+					.click('div[id=sg_game_search_arrow]')
+					.wait(1200)
+					.click('tbody[id=sg_game_table_content] div[id*=test_game_] img[src*=guestlogin]')
+					.waitWindowLoad()
+					.currentWindow()
+					.then(function(currentwindow){
+						nightmare
+							.goto(currentwindow.url)
+							.evaluate(function() {
+								return document.getElementById("ifm").src;
+							})
+							.then(function(gameurl) {
+								console.log("Opening Game: " + vargameid)
+								if(debug) { console.log("[DEBUG] Game Url:" +  gameurl); }
+								var indexdays= [];
+								resolve(gameurl)
+							});
+					});
+			});
+		},
+		GotoGame: function(gameurl) {
+			return new Promise(function(resolve, reject) {
+				nightmare
+					.goto(gameurl)
+					.waitWindowLoad()
+					.wait(2500)
+					.then(function() {
+						if(debug) { console.log("[DEBUG] Game opened"); }
+						resolve("Game opened");
+					});
+			});
+		},
+		OpenPaper: function() {
+			console.log("[DEBUG] Opening Paper")
+			return new Promise(function(resolve, reject) {
+				nightmare
+					.wait(2500)
+					.click('div[id=func_btn_newspaper]')
+					.then(function() {
+						resolve();
+						FillPaper("Done");
+					})
+					.catch(function(error) {
+						reject("Error");
+					});
+			});
+		},
+		FillPaper: function() {
+			return new Promise(function(resolve, reject) {
+				nightmare
+					.wait(2000)
+					.evaluate(function() {
+						return document.querySelector('input#func_newspaper_day_tf').value;
+					})
+					.then(function(totaldays) {
+						if(debug) { console.log("Total Days:" +  totaldays); }
+						indexdays = [];
+						RunNext(1, totaldays, indexdays);
+					})
+					.catch(function(error) {
+						handleError(error);
+						FillPaper();
+					});
+			});
+		},
+	}
+})();
--- js/supremarket/supremarket.js
+++ js/supremarket/supremarket.js
@@ -1,8 +1,70 @@
+// Requires
+var Nightmare = require('nightmare')
+require('nightmare-window-manager')(Nightmare);
+
+// Settings
+var debug = true;
+var showWindow = true;
+
+// Importt the config file
+var configFile = require('./config');
+var screenshotDeal = configFile.screenshotDeal;
+var username = configFile.username;
+var password = configFile.password;
+var gameid = configFile.gameid;
+
+// App data
+var suploginurl = "http://www.supremacy1914.com/index.php?id=250&L=2";
+var useragent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36";
+var nightmare_opts = {
+	show: showWindow,
+	waitTimeout: 10000,
+	gotoTimeout: 5000,
+	loadTimeout: 5000,
+};
+
+// Prints welkome message
+console.log("Supremacy1914 SupreMarket by Joost Sijm");
+
+// Settings check
+if (gameid.length > 7) {
+	console.log("Error: length of game ID is too long to be valid.");
+	console.log("Please use a valid game ID");
+	process.exit();
+}
+// Creating variables
+var products = ["Graan", "Vis", "IJzererts", "Hout", "Steenkool", "Olie", "Gas"];
 var buytop = [];
 var selltop = [];
 var allcolumbs = [];
-var products = ["Graan", "Vis", "IJzererts", "Hout", "Steenkool", "Olie", "Gas"];
 var deals = [];
+
+// Handle errors
+function handleError(err) {
+	if(debug) {
+		console.log("[DEBUG] Error: " JSON.stringify(err));
+	}
+	return err;
+}
+
+// Starting program
+var nightmare = Nightmare(nightmare_opts);
+nigtmare.useragent(username);
+
+Login();
+
+function Login {
+	if(debug) { console.log("[DEBUG] Handle login page: " + username); }
+	nightmare
+		.windowManager()
+		.goto(suploginurl)
+		.exists('form[id=sg_login_form]')
+		.then(function(nologin) {
+			if (nologin) {
+				if(debug) { console.log("[DEBUG] Account not logged in" e); }
+			}
+		})
+}
 var inside = iframeRef( document.getElementById('ifm') )
 if ($('.buy_table tr').length === 0) {
 	$("#func_btn_stockmarket").click();
