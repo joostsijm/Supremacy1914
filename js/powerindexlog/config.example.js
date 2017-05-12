0df91247da3b0e5e7c169040d999ee846409c060
JoostSijm <joostsijm@gmail.com>
2017-05-12 17:19:45 +0200

    Move config to example file

--- js/powerindexlog/config.example.js
+++ js/powerindexlog/config.example.js
@@ -1,15 +1,15 @@
 var config = {
 	// Options
 	// Saves a screenshot even if registration fails when set to true
-	screenshotResult:true,
+	screenshotResult: true,
 
 	// Game Options 
 	// Your supremacy1914 username. 
-	username:"REPLACE",
+	username: "example",
 	// This is password of your supremacy1914 account.
-	password:"REPLACE",
+	password: "example",
 	// Game ID to get the power index from.
-	gameid:"REPLACE"
+	gameid: "1111111",
 };
 
 module.exports = config;
